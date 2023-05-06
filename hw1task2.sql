676dd52763f03c4f58c98269e44fd57460eac664
ustinova alisa <ustinova.aa@phystech.edu>
2023-05-06 20:08:21 +0000

    Delete hw1task2.sql

--- hw1task2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS hw(
-                                 id INTEGER,
-                                 height FLOAT4,
-                                 weight FLOAT4
-)
-
-select (weight*703)/(height^2) as bmi
-    from hw;
